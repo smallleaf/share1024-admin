50096d257e8fc3d94c7bc843074feaa865928eca
yesheng <578986218@qq.com>
2017-01-12 17:27:56 +0800

    share1024-admin

---
+++ README.md
@@ -0,0 +1,3 @@
+## Share1024-admin 开发版本
+## 网站运维后台 [www.share1024.com](http://www.share1024.com/)
+### v 1.0.0 
---
+++ share1024-admin-model/src/main/java/org/share1024/admin/model/App.java
@@ -0,0 +1,13 @@
+package org.share1024.admin.model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
